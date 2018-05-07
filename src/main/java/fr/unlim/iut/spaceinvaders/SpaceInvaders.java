f588b3d6e82be04ab50e27de32eeaa63d31d1d5a
Quentin Depierre <depierre.q@gmail.com>
2018-05-07 15:16:11 +0200

    Refactoring création vaisseau

--- src/main/java/fr/unlim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unlim/iut/spaceinvaders/SpaceInvaders.java
@@ -2,7 +2,10 @@ package fr.unlim.iut.spaceinvaders;
 
 public class SpaceInvaders {
 
-    int longueur;
+    private static final char MARQUE_FIN_LIGNE = '\n';
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V';
+	int longueur;
     int hauteur;
     Vaisseau vaisseau;
 
@@ -14,22 +17,39 @@ public class SpaceInvaders {
     
     @Override
 	public String toString() {
+		return recupererEspaceJeuDansChaineASCII();
+	}
+
+
+	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
 			for (int x = 0; x < longueur; x++) {
-				if (this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y))
-					espaceDeJeu.append('V');
-				else
-					espaceDeJeu.append('.');
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
-			espaceDeJeu.append('\n');
+			espaceDeJeu.append(MARQUE_FIN_LIGNE);
 		}
 		return espaceDeJeu.toString();
 	}
 
 
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque=MARQUE_VAISSEAU;
+		else
+		      marque=MARQUE_VIDE;
+		return marque;
+	}
+
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+
 	private boolean aUnVaisseau() {
-		return this.vaisseau!=null;
+		return vaisseau!=null;
 	}
 
 
