5d83ca7f90052701e91b7bd18e883131b669f49c
Quentin Depierre <depierre.q@gmail.com>
2018-05-14 14:36:53 +0200

    deplacement vers la droite

--- src/main/java/fr/unlim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unlim/iut/spaceinvaders/SpaceInvaders.java
@@ -69,6 +69,11 @@ public class SpaceInvaders {
 	private boolean estDansEspaceJeu(int x, int y) {
 		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
+
+
+	public void deplacerVaisseauVersLaDroite() {
+		vaisseau.seDeplacerVersLaDroite();
+	}
 	
 	
 
--- src/test/java/fr/unlim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unlim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -82,7 +82,24 @@ public class SpaceInvadersTest {
 			
 	}
    
-   
+   @Test
+   public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
+   	spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+
+   	spaceinvaders.deplacerVaisseauVersLaDroite();
+   	
+   	assertEquals("" + 
+   	"...............\n" + 
+   	"...............\n" +
+   	"...............\n" + 
+   	"...............\n" + 
+   	"...............\n" + 
+   	"...............\n" + 
+   	"...............\n" + 
+   	"...............\n" + 
+   	"...............\n" + 
+   	"........V......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+   }
    
    
    
